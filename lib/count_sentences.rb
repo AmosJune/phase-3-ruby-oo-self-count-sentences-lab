178260b701c67b249e0ad7b4ad196072b8f66acd
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-08-22 11:15:15 +0300

    commit tree 9115e346a00e954438404a4165b5e57ecdca3c7b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence = 0
+    punctuation = [".", "?", "!"]
+    self.chars.each.with_index do |char, index|
+      if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+        sentence += 1
+      end
+    end
+      sentence
   end
 end
